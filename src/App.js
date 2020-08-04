68b1acedf32ca4c603686e06464882b11e0c9c6f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-04 11:09:01 -0400

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from 'react'
+import { connect } from 'react-redux'
+import './css/NavBar.scss'
+import './css/App.scss'
+import './css/mediaqueries.scss'
+import { Route, Switch, Redirect } from "react-router-dom"
+import CurrentUserProfilePage from './containers/CurrentUserProfilePage'
+import HomePage from './containers/HomePage'
+import UsersPage from './containers/UsersPage'
+import { fetchProfile, fetchUsers, getExchanges } from './redux/actions'
+import EditProfilePage from './containers/EditProfilePage'
+import MessagesContainer from './containers/MessagesContainer'
+import UserProfilePage from './containers/UserProfilePage'
+import AboutPage from './components/AboutPage'
+// import NavBar from './components/NavBar'
+import LoggedOutNavBar from './components/LoggedOutNavBar'
+import LoggedInNavBar from './components/LoggedInNavBar'
+import { Sticky } from 'semantic-ui-react'
+// import _ from 'lodash'
+import Radium, { StyleRoot } from 'radium'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  contextRef = createRef()
+  
+  componentDidMount() {
+    if (localStorage.jwt) {
+      this.props.fetchProfile()
+      this.props.fetchUsers()  
+    } else {
+      localStorage.clear()
+    }
+  }
+
+  fetchExchanges = () => {
+    fetch('http://localhost:3000/api/v1/findExchanges', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${localStorage.jwt}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({id: this.props.currentUser.id})
+    })
+    .then(resp => {
+      return resp.json()
+    })
+    .then(data => {
+      this.props.getExchanges(data)
+    })
+    .catch(e => console.log(e))
+  }
+  
+  fetchReviews = () => {
+    // debugger
+    fetch('http://localhost:3000/api/v1/reviews', {
+      headers: {
+        'Authorization': `Bearer ${localStorage.jwt}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(resp => console.log(resp))
+  }
+  
+  render() {
+
+    // debugger
+    
+    if (this.props.currentUser) {
+      this.fetchExchanges()
+      this.fetchReviews()
+    }
+
+    return (
+      <StyleRoot>
+        <div className="App" ref={this.contextRef}>
+          <Sticky context={this.contextRef} fluid>
+          {!localStorage.jwt ? 
+          <LoggedOutNavBar attached='top' 
+            tabular 
+            style={{ backgroundColor: '#246a92', 
+            paddingTop: '1em' }} /> 
+            :
+          <LoggedInNavBar attached='top' 
+            tabular 
+            style={{ backgroundColor: '#246a92', paddingTop: '1em' }} />}
+          </Sticky>
+        <Switch>
+          <Route exact path='/'>
+            {!localStorage.jwt ? 
+            <div className='mainPage'>< HomePage /></div> : <div className='mainPage'>< CurrentUserProfilePage /></div>}
+          </Route>
+
+          <Route exact path='/home'>
+            {!localStorage.jwt ? 
+            <div className='mainPage'>< HomePage /></div> : <div className='mainPage'>< CurrentUserProfilePage /></div>}
+          </Route>
+
+          <Route exact strict path='/users'>
+            <div className='mainPage'><UsersPage /></div>
+          </Route>
+
+          <Route exact strict path='/about'>
+            <div className='mainPage'><AboutPage /></div>
+          </Route>
+          
+          <Route path={`/users/${this.props.currentUser.username}/edit`}>
+            <div className='userEditPage'><EditProfilePage /></div>
+          </Route>
+
+          <Route exact strict path='/users/:username'>
+            <div className='mainPage'><UserProfilePage/></div>
+          </Route>
+
+          <Route exact strict path='/messages'>
+            {!localStorage.jwt
+              ? <Redirect to='/login'/>
+                : <div className='mainPage'><MessagesContainer /></div>}
+          </Route>
+          <Redirect from='*' to='/' />
+        </Switch>
+        </div>
+      </StyleRoot>
+    )
+  }
 }
 
-export default App;
+const mapSTP = state => {
+  return {currentUser: state.currentUser}
+}
+
+const mapDTP = dispatch => {
+  return {
+    fetchProfile: user => dispatch(fetchProfile(user)),
+    fetchUsers: users => dispatch(fetchUsers(users)),
+    getExchanges: exchanges => dispatch(getExchanges(exchanges)),
+    // later add and write an action for:
+    getReviews: reviews => dispatch({type: 'GET_REVIEWS', payload: reviews})
+  }
+}
+
+export default connect(mapSTP, mapDTP)(Radium(App))
